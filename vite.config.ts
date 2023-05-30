7c2e859ca6453645eb68cf6295cb8afcf293cedc
Laura Laukkanen <laura.laukkanen@innofactor.com>
2023-05-30 05:44:28 +0300

    edited base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/accessibility-challenges/',
   plugins: [vue()],
 })
